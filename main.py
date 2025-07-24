49fc894ab3e486e4af245e052a8d48e8082b91bf
kar1-ka <kdrlosa@hotmail.com>
2025-07-25 00:03:15 +0100

    function added to check num in string

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("greetings boots")
+def main():
+    path = "books/frankenstein.txt"
+    words = get_book_text(path)
+    num_text = num_in_string(words)
+    print (f"{num_text} words found in the document")
+    
+def get_book_text(path):
+
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+    
+def num_in_string(words): 
+    num_in_text = words.split()
+    num = len(num_in_text)
+    return num
+
+
+
+    
+main()
