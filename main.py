1bc61892b28cd0eecc08cd98db2d3bade984e047
kar1-ka <kdrlosa@hotmail.com>
2025-08-19 15:55:40 +0100

    added descriptions

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from stats import get_num_words, get_char_list
 def main():
-    path = "books/frankenstein.txt" #got the path here
+    path = "books/frankenstein.txt" # got the path here
     words = get_book_text(path) # function calling variable for pathfile
     num_text = get_num_words(words) # function in stats, calling pathfile to variable for number of words
-    charac = get_char_list(words) # fucks it need to iterate it
-    print (f"{num_text} words found in the document")
-    print (f"{charac}")
+    charac = get_char_list(words) # put dict in variable from stats.py
+    print (f"{num_text} words found in the document") # print the num of words in document
+    print (f"{charac}") # print the dict
 
 def get_book_text(path):
     
     with open(path) as f:
-        file_contents = f.read()
+        file_contents = f.read() 
         return file_contents
     
 
@@ -18,4 +18,4 @@ def get_book_text(path):
 
 
     
-main()
+main() # call main operation
--- stats.py
+++ stats.py
@@ -3,15 +3,15 @@ def get_num_words(words):
     num = len(num_in_text)
     return num
 
-def get_char_list (alphabets):
-    chara_dict = {}
-    for characters in alphabets:
-        lowercase_chara = characters.lower()
-        if lowercase_chara not in chara_dict:
-            chara_dict[lowercase_chara] = 1
-        elif lowercase_chara in chara_dict:
-            chara_dict[lowercase_chara] +=1
-    return chara_dict
+def get_char_list (alphabets): # words "string" in function
+    chara_dict = {}  # start new dict
+    for characters in alphabets: # words in full book string
+        lowercase_chara = characters.lower() #lowercase all words
+        if lowercase_chara not in chara_dict: # if lowercase charachters not in dictionary
+            chara_dict[lowercase_chara] = 1 # add the key as chara and add the value as +1
+        elif lowercase_chara in chara_dict: # else if in the dictionary
+            chara_dict[lowercase_chara] +=1 # add the value
+    return chara_dict # return the dictionary with key and values for all chara
         
 
         
